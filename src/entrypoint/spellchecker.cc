6feed172912067145786d0a290b095a86ce2c586
David Bensabat <david.bensabat@epita.fr>
2018-08-09 22:31:27 +0200

    Add multiple line management

--- src/entrypoint/spellchecker.cc
+++ src/entrypoint/spellchecker.cc
@@ -10,7 +10,6 @@ using namespace std;
 
 void print_node_vect(vector<tuple<string, size_t, size_t>> results_vect)
 {
-    //cout << "This vector contained " << results_vect.size() << " words:\n" << endl;
     cout << "[";
     char commat = '\0';
     for(auto tuple : results_vect)
@@ -34,25 +33,47 @@ int main(int argc, char** argv)
     else
     {
         // Deserialize
-        //cout << "\nDeserializing trie" << endl;
-
         string dict_path = argv[1];
-        // cout << dict_path << endl;
         ifstream input_stream(dict_path, ios::binary);
         auto *deserialized_trie = Patricia_trie::deserialize(input_stream);
         input_stream.close();
-        //deserialized_trie->pretty_printer(deserialized_trie);
 
-        // Getting "approx dist word" by pipe
+
         string pipe_approx, pipe_dist_max, pipe_word;
         cin >> pipe_approx >> pipe_dist_max >> pipe_word;
+        int flag = pipe_approx.size();
+        int i = 0;
 
-        // Launch the search
-        //cout << "\nLaunch the search on deserialise:" << endl;
-        auto results_vect = search_ptrie_approx(deserialized_trie, pipe_word, atoi(pipe_dist_max.c_str()));
+        while (flag != 0 && i < 4)
+        {
+            // Launch the search
+            auto results_vect = search_ptrie_approx(deserialized_trie, pipe_word, atoi(pipe_dist_max.c_str()));
 
-        // Print the results
-        print_node_vect(results_vect);
+            // Print the results
+            print_node_vect(results_vect);
+
+            // Getting "approx dist word" by pipe
+            string pipe_approx;
+            string new_line;
+            cin >> new_line;
+
+            // cat style
+            if (new_line == "approx")
+            {
+                pipe_approx = new_line;
+                cin >> pipe_dist_max >> pipe_word >> new_line;
+            }
+
+            // echo style
+            else
+            {
+                cin >> pipe_approx >> pipe_dist_max >> pipe_word >> new_line;
+            }
+            
+            flag = pipe_approx.size();
+
+            i++;
+        }
 
         return 0;
     }
